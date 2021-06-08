e60dafbb1271183332711fb76ed9ea489862aa5d
Amey Joshi <ameyjoshi2001rj@gmail.com>
2021-06-08 21:22:58 +0530

    function update.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Akshay Saini Namste javascript Ep 4
 var x = 1;
+
 a();
 b();
-console.log(x);
 
-const f1 = ()=>{
-     console.log("Hey There!");
+console.log(x);
+getName();
+function getName(){
+     console.log("Amey");
 }
 
 
